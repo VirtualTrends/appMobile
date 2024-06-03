375eee6a0f98183cdb2a9c45290cec9352b3f652
Agostina <ago.95fiore@hotmail.com>
2024-06-02 23:51:25 -0300

    AF subo cracion de Activity ver turnos

---
+++ ANDROID/app/src/main/java/com/example/virtualtrendsmovile/actividades/VerTurnoActivity.java
@@ -0,0 +1,16 @@
+package com.example.virtualtrendsmovile.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.virtualtrendsmovile.R;
+
+public class VerTurnoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_turno);
+    }
+}
